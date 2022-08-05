e42e61130315873123b983b6e4f8c17b0a97d240
dmtrsmlk <dmytro.smlk@gmail.com>
2022-08-05 13:42:12 +0300

    Project enchanced

--- js/script.js
+++ js/script.js
@@ -1,130 +1,178 @@
-fetch('http://api.open-meteo.com/v1/forecast?latitude=50.4422&longitude=30.5367&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&timezone=Europe%2FMoscow')
-    .then(function (resp) {return resp.json()})
-    .then(function (data){
-        console.log(data);
-        document.querySelector('.city').textContent = data.latitude + '/' + data.longitude
-        for(let i = 0; i < 7; i++){
-            document.querySelector(`.card-title${i}`).textContent = moment(data.daily.time[i]).format('MMMM Do YYYY')
-            document.querySelector(`.card-text${i}`).textContent = data.daily.temperature_2m_max[i] + '°C' + ' / ' +data.daily.temperature_2m_min[i] + '°C'
-            if(data.daily.weathercode[i] == 0) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Clear Sky'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/clear.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 1) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Mainly clear'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/mainly_clear.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 2) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Partly Cloudy'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/mainly_clear.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 3) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Overcast'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/overcast.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 45) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Fog'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/fog.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 48) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Depos Rime Fog'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/fog.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 51) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Light Drizzle'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/drizzle.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 53) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Moderate Drizzle'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/drizzle.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 55) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Dense Drizzle'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/drizzle.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 56) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Light Freezing Drizzle'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/freezing_drizzle.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 57) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Dense Freezing Drizzle'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/freezing_drizzle.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 61) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Slight Rain'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/rain.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 63) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Moderate Rain'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/rain.png" alt="" style="width: 125px">';
+let codes = {
+    0:{
+        pname: "Clear",
+        src: "src/clear.png",
+        color: "darkorange"
+    },
+    1: {
+        pname: "Mainly clear",
+        src: "src/mainly_clear.png",
+        color: "blue"
+    },
+    2: {
+        pname: "Partly cloudy",
+        src: "src/mainly_clear.png",
+        color: "gray"
+    },
+    3: {
+        pname: "Overcast",
+        src: "src/overcast.png",
+        color: "gray"
 
-            }
-            else if(data.daily.weathercode[i] == 65) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Heavy Rain'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/rain.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 66) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Light Freezing Rain'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/freezing_rain.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 67) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Heavy Freezing Rain'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/freezing_rain.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 71) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Slight Snowfall'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/snow.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 73) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Moderate Snowfall'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/snow.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 75) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Heavy Snowfall'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/snow.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 77) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Snow Grains'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/snow_grain.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 80) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Slight Rain Showers'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/rain_shower.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 81) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Moderate Rain Showers'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/rain_shower.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 82) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Violent Rain Showers'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/rain_shower.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 85) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Slight Snow Showers'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/snow_shower.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 86) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Heavy Snow Showers'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/snow_shower.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 95) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Thunderstorm'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/thunderstorm.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 96) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Thunderstorm with slight hail'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/thunderstorm.png" alt="" style="width: 125px">';
-            }
-            else if(data.daily.weathercode[i] == 99) {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'Thunderstorm with heavy hail'
-                document.querySelector(`.w-img${i}`).innerHTML = '<img src = "src/thunderstorm.png" alt="" style="width: 125px">';
-            }
-                else {
-                document.querySelector(`.card-subtitle${i}`).textContent = 'no data'
-            }
-        }
+    },
+    45: {
+        pname: "Fog",
+        src: "src/fog.png",
+        color: "dimgray"
+    },
+    48: {
+        pname: "Depositing rime fog",
+        src: "src/fog.png",
+        color: "dimgray"
+    },
+    51: {
+        pname: "Light drizzle",
+        src: "src/drizzle.png",
+        color: "cadetblue"
 
+    },
+    53: {
+        pname: "Moderate drizzle",
+        src: "src/drizzle.png",
+        color: "cadetblue"
+    },
+    55: {
+        pname: "Dense drizzle",
+        src: "src/drizzle.png",
+        color: "cadetblue"
+    },
+    56: {
+        pname: "Light freezing drizzle",
+        src: "src/freezing_drizzle.png",
+        color: "cornflowerblue"
+    },
+    57: {
+        pname: "Dense freezing drizzle",
+        src: "src/freezing_drizzle.png",
+        color: "cornflowerblue"
+    },
+    61: {
+        pname: "Slight rain",
+        src: "src/rain.png",
+        color: "mediumblue"
+    },
+    63: {
+        pname: "Moderate rain",
+        src: "src/rain.png",
+        color: "mediumblue"
+    },
+    65: {
+        pname: "Heavy rain",
+        src: "src/rain.png",
+        color: "mediumblue"
+    },
+    66: {
+        pname: "Light freezing rain",
+        src: "src/freezing_rain.png",
+        color: "royalblue"
+    },
+    67: {
+        pname: "Heavy freezing rain",
+        src: "src/freezing_rain.png",
+        color: "royalblue"
+    },
+    71: {
+        pname: "Slight snow fall",
+        src: "src/snow.png",
+        color: "powderblue"
+    },
+    73: {
+        pname: "Moderate snow fall",
+        src: "src/snow.png",
+        color: "powderblue"
+    },
+    75: {
+        pname: "Heavy snow fall",
+        src: "src/snow.png",
+        color: "powderblue"
+    },
+    77: {
+        pname: "Snow grains",
+        src: "src/snow_grains.png",
+        color: "mediumslateblue"
+    },
+    80: {
+        pname: "Slight rain showers",
+        src: "src/rain_shower.png",
+        color: "blue"
+    },
+    81: {
+        pname: "Moderate rain showers",
+        src: "src/rain_shower.png",
+        color: "blue"
 
-    }).catch(function () {
+    },
+    82: {
+        pname: "Heavy rain showers",
+        src: "src/rain_shower.png",
+        color: "blue"
+    },
+    85:{
+        pname: "Slight snow showers",
+        src: "src/snow_shower.png",
+        color: "deepskyblue"
+    },
+    86: {
+        pname: "Heavy snow showers",
+        src: "src/snow_shower.png",
+        color: "deepskyblue"
+    },
+    95: {
+        pname: "Slight to moderate thunderstorm",
+        src: "src/thunderstorm.png",
+        color: "midnightblue"
+    },
+    96: {
+        pname: "Thunderstorm with slight hail",
+        src: "src/thunderstorm.png",
+        color: "midnightblue"
+    },
+    99: {
+        pname: "Thunderstorm with heavy hail",
+        src: "src/thunderstorm.png",
+        color: "midnightblue"
+    }
+};
+function runQ(a, b) {
+    let latitude = a
+    let longitude = b
+    let attributes = 'weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min'
+    let timezone = 'Europe'
+    let region = 'Moscow'
+    let apiURL = `http://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=${attributes}&timezone=${timezone}%2F${region}`
 
-});
+    fetch(apiURL)
+        .then(function (resp) {
+            return resp.json()
+        })
+        .then(function (data) {
+            for (let i = 0; i < 100; i++) {
+                const subtitle = document.querySelector(`.card-subtitle${i}`)
+                const img = document.querySelector(`.w-img${i}`)
+                const title = document.querySelector(`.card-title${i}`)
+                const text = document.querySelector(`.card-text${i}`)
+
+                title.textContent = moment(data.daily.time[i]).format('MMMM Do YYYY')
+                text.textContent = data.daily.temperature_2m_max[i] + '°C' + ' / ' + data.daily.temperature_2m_min[i] + '°C'
+                for (let key in codes) {
+                    if (data.daily.weathercode[i] == key) {
+                        subtitle.textContent = codes[key].pname
+                        subtitle.style.color = codes[key].color
+                        img.innerHTML = '<img src = "' + codes[key].src + '" style="width: 125px">';
+                    }
+                }
+            }
+        }).catch(function () {
+
+    });
+}
